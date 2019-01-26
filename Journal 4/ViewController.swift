843ce508a31f38dfb0a33f53de304b17bfcbb68e
Ryan Greenburg <ryangreenburg@mac.com>
2019-01-26 10:07:11 -0700

    Initial Commit

---
+++ Journal 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal 4
+//
+//  Created by RYAN GREENBURG on 1/26/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
